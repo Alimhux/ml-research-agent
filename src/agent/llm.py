c6fb79b4bee327ce1f79e59394d317ddb66ece70
Alimhux <slavalegoman@mail.ru>
2026-04-21 00:58:15 +0300

    added main schemas and tool for searching papers on arxiv.org

--- src/agent/llm.py
+++ src/agent/llm.py
@@ -2,17 +2,16 @@ import os
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
-
 load_dotenv()   
 
 
 def get_llm(model_name: str = "yandexgpt-5.1/latest", temperature: float = 0) -> ChatOpenAI: 
-    folder_id = os.environ["YANDEX_CLOUD_FOLDER"]
-    api_key = os.environ["YANDEX_CLOUD_API_KEY"]
+    folder_id = os.getenv("YANDEX_CLOUD_FOLDER")
+    api_key = os.getenv("YANDEX_CLOUD_API_KEY")
 
     return ChatOpenAI(
         model=f"gpt://{folder_id}/{model_name}",
-        temperature=temperature,
+        temperature=temperature,    
         api_key=api_key,
         base_url="https://ai.api.cloud.yandex.net/v1"
     )
